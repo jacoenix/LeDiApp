3e96579ede4a8965dce54b1f533577d8319bb5c0
jacoenix <160466782+jacoenix@users.noreply.github.com>
2024-08-25 14:32:22 +0200

    Update USeeder.php
    etc

--- database/seeders/USeeder.php
+++ database/seeders/USeeder.php
@@ -21,14 +21,6 @@ class USeeder extends Seeder
             'password' => '$2y$10$KtcSx2atgA1YJ8ZUZ7Wo8.n0g57FPFPwELrxaXYsUZltztEO/KtZu',
             'remember_token' => Str::random(10)
         ]);
-
-        User::create([
-            'name' => 'superadmin',
-            'email' => 'ledi@dev.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$ycP5KufGNysVN7obwT64w.UhGWDM1XBJot8cbkvE7kNDkAkPtoOVi',
-            'remember_token' => Str::random(10)
-        ]);
     }
 }
 
